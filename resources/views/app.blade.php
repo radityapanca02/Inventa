d9b7b92e8f9635b6d1e09357c1de622aa81be87f
radityapanca02 <radityapanca02@gmail.com>
2025-10-07 22:52:28 +0700

    Add Bab 3

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,6 +7,8 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
+        <link rel="shortcut icon" href="{{ asset('image/logo.png') }}" type="image/x-icon">
+
         <!-- Scripts -->
         @routes
         @viteReactRefresh
